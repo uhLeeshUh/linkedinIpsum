c8a0948ea68df97efe8607950babb804e0286bf4
Alicia <alicia.underhill@gmail.com>
2020-03-18 17:31:04 -0400

    Make this a typescript app and add tsconfig.json

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,5 @@
-const path = require("path");
-const express = require("express");
+import path from "path";
+import express from "express";
 
 const app = express();
 const DIST_DIR = __dirname;
@@ -7,7 +7,7 @@ const HTML_FILE = path.join(DIST_DIR, "index.html");
 
 app.use(express.static(DIST_DIR));
 
-app.get("*", (req, res) => {
+app.get("*", (req: express.Request, res: express.Response) => {
   res.sendFile(HTML_FILE);
 });
 
