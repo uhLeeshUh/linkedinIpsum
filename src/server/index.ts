c17217e68b87eb82b8b3bf204d000807f88160df
Alicia <alicia.underhill@gmail.com>
2020-03-18 19:38:15 -0400

    Update dist path in server index file

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,7 +2,7 @@ import path from "path";
 import express from "express";
 
 const app = express();
-const DIST_DIR = __dirname;
+const DIST_DIR = path.resolve(__dirname, "..", "..", "dist");
 const HTML_FILE = path.join(DIST_DIR, "index.html");
 
 app.use(express.static(DIST_DIR));
