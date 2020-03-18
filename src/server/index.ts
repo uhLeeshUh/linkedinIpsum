6f3dfab9b0823c8b3c38e9b6c06bd7848fd64f67
Alicia <alicia.underhill@gmail.com>
2020-03-18 16:52:09 -0400

    Create server index file

---
+++ src/server/index.ts
@@ -0,0 +1,18 @@
+import path from "path";
+import express from "express";
+
+const app = express();
+const DIST_DIR = __dirname;
+const HTML_FILE = path.join(DIST_DIR, "index.html");
+
+app.use(express.static(DIST_DIR));
+
+app.get("*", (req: express.Request, res: express.Response) => {
+  res.sendFile(HTML_FILE);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`App listening to ${PORT}....`);
+  console.log("Press Ctrl+C to quit.");
+});
