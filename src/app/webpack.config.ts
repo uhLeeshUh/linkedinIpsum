cb2bd18b8d0dca595cbbe9ebb6bf7517510eb1c4
Alicia <alicia.underhill@gmail.com>
2020-03-20 14:55:14 -0400

    Set up dev mode with concurrent frontend and backend servers

--- src/app/webpack.config.ts
+++ src/app/webpack.config.ts
@@ -1,9 +1,17 @@
 import HtmlWebPackPlugin from "html-webpack-plugin";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+import path from "path";
 
 const isProduction = process.env.NODE_ENV === "production";
+const outputDir = "dist";
 
 module.exports = {
   mode: isProduction ? "production" : "development",
+  entry: "./src/app/index.tsx",
+  output: {
+    path: path.join(__dirname, outputDir),
+    filename: "bundle.js",
+  },
   module: {
     rules: [
       {
@@ -27,11 +35,13 @@ module.exports = {
   resolve: {
     extensions: [".js", ".ts", ".tsx"],
   },
-  entry: "./src/app/index.tsx",
+  devtool: isProduction ? false : "inline-source-map", // maps compiled code back to original source code
   devServer: {
-    historyApiFallback: true,
+    port: 3000,
+    historyApiFallback: true, // https://stackoverflow.com/questions/26203725/how-to-allow-for-webpack-dev-server-to-allow-entry-points-from-react-router
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebPackPlugin({
       template: "./src/server/index.html",
       filename: "./index.html",
--- src/server/index.ts
+++ src/server/index.ts
@@ -2,7 +2,7 @@ import path from "path";
 import express from "express";
 
 const app = express();
-const DIST_DIR = path.resolve(__dirname, "..", "..", "dist");
+const DIST_DIR = path.resolve(__dirname, "..", "./app", "dist");
 const HTML_FILE = path.join(DIST_DIR, "index.html");
 
 app.use(express.static(DIST_DIR));
@@ -11,7 +11,7 @@ app.get("*", (req: express.Request, res: express.Response) => {
   res.sendFile(HTML_FILE);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`App listening to ${PORT}....`);
   console.log("Press Ctrl+C to quit.");
