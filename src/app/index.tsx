309504b9e60602ed2345689eb71443421317bc69
Alicia <alicia.underhill@gmail.com>
2020-03-23 14:03:39 -0400

    Wire up Apollo Client into React app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import routes from "./routes";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+
+const client = new ApolloClient();
 
 const App = () => {
-  return <BrowserRouter>{routes}</BrowserRouter>;
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>{routes}</BrowserRouter>
+    </ApolloProvider>
+  );
 };
 
 document.addEventListener("DOMContentLoaded", () => {
