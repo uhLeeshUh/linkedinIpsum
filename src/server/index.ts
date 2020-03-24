2f912322011d2d609387136ff1ac16ef660be55f
Alicia <alicia.underhill@gmail.com>
2020-03-24 18:50:23 -0400

    Wire up knex and objection in server index file

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,16 +2,28 @@ import path from "path";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import schema from "./graphql/make-executable-schema";
+import knexConfig from "./knex/db";
+import Knex from "knex";
+import { Model } from "objection";
 
 const app = express();
+
 const DIST_DIR = path.resolve(__dirname, "..", "./app", "dist");
 const HTML_FILE = path.join(DIST_DIR, "index.html");
 
+// compiled static asset bundle (javascript, css) to run in client browser
 app.use(express.static(DIST_DIR));
 
+// serve HTML file for React to plug into
 app.get("*", (req: express.Request, res: express.Response) => {
   res.sendFile(HTML_FILE);
 });
+
+// wire up connection to the database
+const knex = Knex(knexConfig);
+
+// wire up Objection to database connection
+Model.knex(knex);
 
 const server = new ApolloServer(schema);
 server.applyMiddleware({ app });
---
+++ src/server/knex/db.ts
@@ -0,0 +1,6 @@
+import knexfile from "./knexfile";
+
+const env = process.env.NODE_ENV || "development";
+const configOptions = knexfile[env];
+
+export default configOptions;
