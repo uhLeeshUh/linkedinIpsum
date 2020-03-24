4577d37b0396d0fdc6c96d5d189e5dba9ff00d3a
Alicia <alicia.underhill@gmail.com>
2020-03-24 16:54:35 -0400

    Write migration to create industry table

---
+++ src/server/knex/migrations/20200324152731_create-industry-table.ts
@@ -0,0 +1,25 @@
+import * as Knex from "knex";
+
+export async function up(knex: Knex): Promise<any> {
+  return knex.schema.hasTable("industry").then(exists => {
+    if (!exists) {
+      return knex.schema.createTable("industry", table => {
+        table.string("id").primary();
+        table.string("name").notNullable();
+
+        // timestamps
+        table.timestamp("createdAt").defaultTo(knex.raw("now()"));
+        table.timestamp("updatedAt").defaultTo(knex.raw("now()"));
+        table.timestamp("deletedAt");
+      });
+    }
+  });
+}
+
+export async function down(knex: Knex): Promise<any> {
+  return knex.schema.hasTable("industry").then(exists => {
+    if (exists) {
+      return knex.schema.dropTable("industry");
+    }
+  });
+}
