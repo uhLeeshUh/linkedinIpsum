c91f5d2bde66533f2dbbaff9411fdaeabbceebb4
Alicia <alicia.underhill@gmail.com>
2020-03-26 18:21:48 -0400

    Write and test Template.getRandomByIndustryId

--- src/server/models/__tests__/template.spec.ts
+++ src/server/models/__tests__/template.spec.ts
@@ -1,6 +1,8 @@
 import { setupTestDb } from "../../helpers/test-helpers";
 import { Transaction, transaction } from "objection";
 import Template from "../template";
+import Industry from "../industry";
+import IndustryTemplate from "../industry-template";
 
 describe("Template model", () => {
   let testDb: ReturnType<typeof setupTestDb>;
@@ -30,6 +32,42 @@ describe("Template model", () => {
       expect(result.deletedAt).toBeNull();
       expect(result.createdAt).not.toBeNull();
       expect(result.updatedAt).not.toBeNull();
+    });
+  });
+
+  describe("getRandomByIndustryId", () => {
+    let industry: Industry;
+    let template0: Template;
+    let template1: Template;
+    let template2: Template;
+
+    beforeEach(async () => {
+      industry = await Industry.create({ name: "my new industry" }, txn);
+
+      template0 = await Template.create(txn);
+      template1 = await Template.create(txn);
+      template2 = await Template.create(txn);
+
+      await IndustryTemplate.create(
+        { industryId: industry.id, templateId: template0.id },
+        txn,
+      );
+      await IndustryTemplate.create(
+        { industryId: industry.id, templateId: template1.id },
+        txn,
+      );
+      await IndustryTemplate.create(
+        { industryId: industry.id, templateId: template2.id },
+        txn,
+      );
+    });
+
+    it("gets a template for which there is a matching industry_template entry", async () => {
+      const result = await Template.getRandomByIndustryId(industry.id, txn);
+
+      expect([template0, template1, template2]).toContainEqual(
+        expect.objectContaining(result),
+      );
     });
   });
 });
--- src/server/models/template.ts
+++ src/server/models/template.ts
@@ -17,4 +17,18 @@ export default class Template extends BaseModel {
   static async create(txn: Transaction): Promise<Template> {
     return this.query(txn).insertAndFetch({});
   }
+
+  static async getRandomByIndustryId(
+    industryId: string,
+    txn: Transaction,
+  ): Promise<Template> {
+    return this.query(txn)
+      .select("template.*")
+      .leftJoin("industry_template as it", builder =>
+        builder.on("template.id", "=", "it.templateId"),
+      )
+      .where({ "it.industryId": industryId })
+      .orderByRaw("random()")
+      .first();
+  }
 }
