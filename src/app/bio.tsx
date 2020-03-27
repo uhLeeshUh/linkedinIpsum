6efc197039c324c7d698c02b5a0c8daa25b793bc
Alicia <alicia.underhill@gmail.com>
2020-03-27 15:15:33 -0400

    Update Bio logic to print retrieved bioText

--- src/app/bio.tsx
+++ src/app/bio.tsx
@@ -2,29 +2,41 @@ import { useQuery } from "@apollo/react-hooks";
 import React from "react";
 import { useParams } from "react-router-dom";
 import getBioQuery from "./graphql/queries/get-bio.graphql";
-
-interface IBio {
-  id: string;
-  name: string;
-  bioText: string;
-}
-
-interface IBioResult {
-  bio: IBio;
-}
+import { IBioData, IBio } from "./graphql/graphql-types";
 
 const Bio = () => {
   const { bioId } = useParams();
-  const { data, loading } = useQuery<IBioResult>(getBioQuery, {
+  const { data, loading, error } = useQuery<IBioData>(getBioQuery, {
     variables: { bioId },
   });
+
   if (loading) return null;
+  if (error) {
+    console.error(`Error loading bio: ${error}`);
+  }
+
   return (
     <>
-      <div>BIO FOR: {data && data.bio ? data.bio.name : "no name"}</div>
-      <div>BIO IS: {data && data.bio ? data.bio.bioText : "not defined"}</div>
+      <div>
+        BIO FOR: {data && data.bio ? data.bio.name : "Business Professional"}
+      </div>
+      <div>
+        BIO IS: {data && data.bio ? assembleBioText(data.bio) : "not defined"}
+      </div>
     </>
   );
+};
+
+const assembleBioText = (bio: IBio) => {
+  let bioText = "";
+  bio.bioChunks.forEach(bioChunk => {
+    const { templateChunk, followingVariable } = bioChunk;
+    bioText += templateChunk.chunkText;
+    if (followingVariable) {
+      bioText += followingVariable.variableText;
+    }
+  });
+  return bioText;
 };
 
 export default Bio;
